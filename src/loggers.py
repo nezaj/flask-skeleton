c9498b017f9987103cda3e8265d4b65f58378806
Joe Averbukh <joeaverbukh@gmail.com>
2014-05-20 14:10:34 -0700

    Add loggers

---
+++ src/loggers.py
@@ -0,0 +1,31 @@
+"""
+Helper functions for configuring loggers.
+"""
+
+import logging
+
+from config import app_config
+
+def configure_sqlalchemy_logger(format_string, level):
+    logger = logging.getLogger('sqlalchemy.engine')
+    logger.setLevel(level)
+    logger.addHandler(get_stderr_handler(format_string, level))
+
+def get_stderr_handler(format_string, level):
+    "Returns a standard error handler with provided format and level"
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter(*format_string))
+    return handler
+
+def get_app_stderr_handler():
+    "Returns a configured stderr_handler"
+    return get_stderr_handler(app_config.STDERR_LOG_FORMAT, app_config.APP_LOG_LEVEL)
+
+def get_stderr_logger():
+    "Returns a Logger object with a configured stderr_handler"
+    logger = logging.getLogger()
+    logger.handlers = []
+    logger.setLevel(app_config.APP_LOG_LEVEL)
+    logger.addHandler(get_app_stderr_handler())
+    return logger
