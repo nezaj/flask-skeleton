76effe3eb9d28a10a970cf75b8cd69806c17f0b1
Joe Averbukh <joeaverbukh@gmail.com>
2014-06-30 19:21:10 -0700

    Minor blueprint fix

--- src/web/routes/home.py
+++ src/web/routes/home.py
@@ -3,7 +3,7 @@ Logic for dashboard related routes
 """
 from flask import Blueprint, render_template
 
-home = Blueprint('main', __name__)
+home = Blueprint('home', __name__)
 
 @home.route('/', methods=['GET'])
 def index():
