e2f794e30818d1bde0de439797936236ac88eb20
Joe Averbukh <joeaverbukh@gmail.com>
2014-07-11 16:08:31 -0700

    Add tests for User and UserPasswordToken

---
+++ src/tests/models/tools.py
@@ -0,0 +1,11 @@
+from data.models import User
+
+def generate_model(cls, cls_attrs):
+    return cls(**cls_attrs)
+
+def generate_user(**kwargs):
+    options = dict(email="mock@example.com",
+                   username="mock",
+                   password="mock")
+    options.update(**kwargs)
+    return generate_model(User, options)
